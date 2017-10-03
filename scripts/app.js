131dc2f44e4a62b3b5b56b415af5d1afee0589b8
Dan Tripp <daniel_tripp@yahoo.com>
2017-10-03 15:25:53 -0400

    orig

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+'use strict'
+$(function(){
+    $('.btn').on('click',function(){
+        //alert('foobar');
+        //$('#lol').find('plist').slideToggle();
+        //$(this).next('.plist').slideToggle();
+        $(this).next('.plist').slideToggle();
+        //$('.plist').slideToggle();
+    })
+});
